b1b30754314ed3911470f8dfc0b42fcedf96064d
Linal180 <amirnisar75@gmail.com>
2024-02-23 18:49:46 +0500

    Auth token empty issue fixed

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,21 +3,27 @@ import { NextFunction, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req: any, res: Response, next: NextFunction) => {
-  const authHeader = req.header('Authorization').split(' ');
+  if(req.header('Authorization')){
+    const authHeader = req.header('Authorization').split(' ');
 
-  if(authHeader.length > 0) {
-    const token = authHeader[1];
+    if(authHeader.length > 0) {
+      const token = authHeader[1];
 
-    if(token)
-      try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY || 'secret');
-        req.user = decoded;
+      if(token){
+        try {
+          const decoded = jwt.verify(token, process.env.SECRET_KEY || 'secret');
+          req.user = decoded;
 
-        next();
-      } catch (error) {
-        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+          next();
+        } catch (error) {
+          return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+        }
+      } else {
+        return res.status(401).json({ message: 'Unauthorized: Missing token' });
       }
     }
+  } else {
+    res.status(401).json({ message: 'Unauthorized: Missing token' });
+  }
 
-  return res.status(401).json({ message: 'Unauthorized: Missing token' });
-};
+}
